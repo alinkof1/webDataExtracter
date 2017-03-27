0bbb39a69e2330b49e815d0747353178c1e5ed9c
Alex Linkoff <alinkof1@binghamton.edu>
2017-03-27 15:48:04 -0400

    edited code for syntax
    not PoC; need to edit XML Parser

--- getQueries.py
+++ getQueries.py
@@ -2,6 +2,16 @@
 #queries a search result through amazon.com's search engine
 #returns an object list of the top results
 
+"""
+	python script load up search results
+		parse through for links to product pages
+			load up individual product 
+	seller
+	rating
+
+	resultscol element in html
+"""
+
 from lxml import html
 import requests
 import re
@@ -11,26 +21,24 @@ XPATH_SALE_PRICE = '//span[contains(@id,"ourprice") or contains(@id,"saleprice")
 XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
 XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
 XPATH_AVAILABILITY = '//div[@id="availability"]//text()'
-NUMQUERY = 5
 REQ = "http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
 #XPATH_NAME = tree.xpath('//div[@title="buyer-name"]/text()')
 
 
 class results(object):
-	product_names = []
-	product_prices = []
-	product_categories = []
-	products = []
-	user_query = []
-	all_queries = []
-
 	def __init__(self):
+		self.product_names = []
+		self.product_prices = []
+		self.product_categories = []
+		self.products = []
+		self.user_query = []
+		self.all_queries = []
 		with open('reqs.txt', 'r') as reqs:
-			user_query = reqs.read()
-			user_query = user_query.split('\n')
+			self.user_query = reqs.read()
+			self.user_query = self.user_query.split('\n')
 
-		for i,w in enumerate(user_query):
+		for i,w in enumerate(self.user_query):
 			if (w):
 				w = re.sub(r"\s+", '+', w)
 				self.all_queries.append(w)
@@ -58,18 +66,4 @@ class results(object):
 if __name__ == "__main__":
 
     amzn = results()
-    quer = []
     print amzn.search_query()
-    #print quer
-
-
-
-"""
-	python script load up search results
-		parse through for links to product pages
-			load up individual product 
-	seller
-	rating
-
-	resultscol element in html
-"""
